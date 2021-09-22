a5dd505f1633a4b5931028e9ffa4b53d0da3fe86
Lacina ZINA <zinalacina@gmail.com>
2021-09-22 03:17:12 -0500

    change the version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="GhTrack",
-    version="0.0.1",
+    version="1.0.0",
     py_modules=["GhTrack"],
     description="An Python module api app",
     long_description=long_description,
